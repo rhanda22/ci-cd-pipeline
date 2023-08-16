cc64f838e0eef22219595293183bc05e95d34b6f
rahul handa <rahulhanda22@gmail.com>
2023-08-15 23:08:59 -0400

    lamda changed

--- lambda/handler.ts
+++ lambda/handler.ts
@@ -3,6 +3,8 @@ export async function handler(event:string, context:string) {
   
   console.log(' Stage Name ::'+ process.env.stage);
 
+  console.log('Adding a update to function');
+
   return{
     body:'Hello from Lambda Function',
     statusCode: 200
