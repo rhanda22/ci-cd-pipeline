d5a3f8503a76b07b266c7d2c3ac348db86bbcfb0
rahul handa <rahulhanda22@gmail.com>
2023-08-18 21:01:22 -0400

    demo change

--- lambda/handler.ts
+++ lambda/handler.ts
@@ -3,7 +3,7 @@ export async function handler(event:string, context:string) {
   
   console.log(' Stage Name ::'+ process.env.stage);
 
-  console.log('Making a change for a demo');
+  console.log('Making a change for a demo to trigger');
 
   return{
     body:'Hello from Lambda Function',
