ef459ed588a3585ad2a012e96f65b22f1b01e3da
rahul handa <rahulhanda22@gmail.com>
2023-09-26 16:51:53 -0400

    trigger 4

--- bin/ci-cd-pipeline.ts
+++ bin/ci-cd-pipeline.ts
@@ -4,8 +4,12 @@ import * as cdk from 'aws-cdk-lib';
 import { CiCdPipelineStack } from '../lib/ci-cd-pipeline-stack';
 
 const app = new cdk.App();
+console.log('trigger a change');
 new CiCdPipelineStack(app, 'CiCdPipelineStack','dev', {
    //env: { account: '276092530129', region: 'us-east-1' },
+
+   
+
 });
 
 app.synth();
--- lambda/handler.ts
+++ lambda/handler.ts
@@ -3,11 +3,11 @@ export async function handler(event:string, context:string) {
   
   console.log(' Stage Name ::'+ process.env.stage);
 
-  console.log('Making a change for a demo to trigger0');
+  console.log('Making a change for a demo to trigger1');
 
   console.log('Making a change for a demo to trigger 2');
 
-  console.log('Making a change for a demo to trigger 1');
+  console.log('Making a change for a demo to trigger 3');
 
 
 
